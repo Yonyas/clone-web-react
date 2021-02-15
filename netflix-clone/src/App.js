a59631685ed0af9e3633e916c1612e336036621c
Jiyon Lee <jiyonlee.d@gmail.com>
2021-02-15 13:51:52 +0900

    다른 카테고리의 영화포스터 클릭시 현재 재생중인 예고편이 안꺼지는 문제 해결

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,33 @@
 import "./App.css";
+import React, { useState } from "react";
 import Row from "./Row";
 import Banner from "./Banner";
 import Nav from "./Nav";
 import requests from "./requests";
+import movieTrailer from "movie-trailer";
 
 function App() {
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [currentMovie, setCurrentMovie] = useState("");
+
+  //when click movie poster
+  const handleClick = (movie, category) => {
+    setTrailerUrl("");
+    movieTrailer(
+      movie?.original_name ||
+        movie?.original_title ||
+        movie?.title ||
+        movie?.name ||
+        "",
+      { id: true }
+    )
+      .then((id) => {
+        setTrailerUrl(id);
+        setCurrentMovie(category); //내가 클릭한 영화 카테고리 ex) NETFLIX ORIGINALS
+      })
+      .catch((error) => console.log(error));
+  };
+
   return (
     <div className="app">
       <Nav />
@@ -12,16 +35,52 @@ function App() {
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.fetchNetflixOriginals}
+        trailerUrl={currentMovie === "NETFLIX ORIGINALS" ? trailerUrl : null}
+        handleClick={handleClick}
         isLargeRow
       />
-
-      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
-      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
-      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        trailerUrl={currentMovie === "Trending Now" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+        trailerUrl={currentMovie === "Top Rated" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        trailerUrl={currentMovie === "Action Movies" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        trailerUrl={currentMovie === "Comedy Movies" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        trailerUrl={currentMovie === "Horror Movies" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        trailerUrl={currentMovie === "Romance Movie" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        trailerUrl={currentMovie === "Documentaries" ? trailerUrl : null}
+        handleClick={handleClick}
+      />
     </div>
   );
 }
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -2,13 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import "./row.css";
 import YouTube from "react-youtube";
-import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({ title, fetchUrl, isLargeRow }) {
+function Row({ title, fetchUrl, isLargeRow, trailerUrl, handleClick }) {
   const [movies, setMovies] = useState([]);
-  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     //useEffect 안에서 async 할때마다 이런식으로 해야함. 함수 만들고 부르기
@@ -30,24 +28,6 @@ function Row({ title, fetchUrl, isLargeRow }) {
     },
   };
 
-  //when click movie poster
-  const handleClick = (movie) => {
-    if (trailerUrl) {
-      setTrailerUrl("");
-    } else {
-      movieTrailer(
-        movie?.original_name ||
-          movie?.original_title ||
-          movie?.title ||
-          movie?.name ||
-          "",
-        { id: true }
-      )
-        .then((id) => setTrailerUrl(id))
-        .catch((error) => console.log(error));
-    }
-  };
-
   return (
     <div className="row">
       <h2>{title}</h2>
@@ -57,7 +37,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
           <img
             key={movie.id}
             onClick={() => {
-              handleClick(movie);
+              handleClick(movie, title);
             }}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
