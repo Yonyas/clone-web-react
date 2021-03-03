b7cbfb5f42b79bc12e69b53c975c339fbbad4586
Jiyon Lee <jiyonlee.d@gmail.com>
2021-03-03 11:42:15 +0900

    header 검색창의 select
    Material-UI Select API 사용, autowidth 사용, 폰트사이즈 수정 위해 크롬에서 클래스명 보고 수정

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
+import Select from "@material-ui/core/Select";
+import NativeSelect from "@material-ui/core/NativeSelect";
 
 function Header() {
+  const [selectedOption, setSelectedOption] = useState("All");
+
   return (
     <nav className="header">
       <div className="header__top">
@@ -18,12 +22,38 @@ function Header() {
           Delever to Yonyas <span>Gangnam 099</span>
         </div>
         <div className="header__search">
-          <select className="header__searchSelect">
-            <option value="All">All</option>
-            <option value="arts-craft">Arts & Craft</option>
-            <option value="automotive">automotive</option>
-            <option value="baby">baby</option>
-          </select>
+          <Select
+            className="header__searchSelect"
+            value={selectedOption}
+            onChange={(e) => {
+              setSelectedOption(e.target.value);
+            }}
+            autoWidth
+            // style={{ width: `${stringWidth + 100}` }}
+          >
+            <option className="header__option" value="All">
+              All
+            </option>
+            <option className="header__option" value="arts-craft">
+              Arts & Craft
+            </option>
+            <option className="header__option" value="automotive">
+              automotive
+            </option>
+            <option className="header__option" value="baby">
+              baby
+            </option>
+            <option className="header__option" value="beauty-personal">
+              Beauty & Personal Care
+            </option>
+            <option className="header__option" value="books">
+              Books
+            </option>
+            <option className="header__option" value="computers">
+              Computers
+            </option>
+          </Select>
+
           <input type="text" className="header__searchInput" />
           <SearchIcon className="header__searchIcon" />
         </div>
