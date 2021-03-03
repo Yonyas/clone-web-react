415a5fcb08ac2e5c74474a46e93f0d3690852079
Jiyon Lee <jiyonlee.d@gmail.com>
2021-03-03 16:41:43 +0900

    주석정리

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -3,7 +3,7 @@ import "./header.css";
 import { Link } from "react-router-dom";
 import SearchIcon from "@material-ui/icons/Search";
 import Select from "@material-ui/core/Select";
-import sprite from "./sprite.png";
+import sprite from "./images/sprite.png";
 
 function Header() {
   const [selectedOption, setSelectedOption] = useState("All");
@@ -70,9 +70,7 @@ function Header() {
         </div>
         {/* 4 Links */}
         <div className="header__nav">
-          {/* 1st link */}
           <div className="header__language"></div>
-          {/* 2nd link */}
           <Link to="/" className="header__link">
             <div className="header__account header__linkblock">
               <span className="">Hello, Yonyas </span>
@@ -81,7 +79,6 @@ function Header() {
               </span>
             </div>
           </Link>
-          {/* 3rd link */}
           <Link to="/" className="header__link">
             <div className="header__order  header__linkblock">
               <span className="">Returns </span>
@@ -90,7 +87,6 @@ function Header() {
               </span>
             </div>
           </Link>
-          {/* 4th link */}
           <Link to="/checkout" className="header__link">
             <div className="header__shoppingCart header__linkblock">
               <img className="header__cartImg" src={sprite} alt="cart"></img>
